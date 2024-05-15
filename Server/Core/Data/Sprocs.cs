f29b2703f3999f5dbc0ca7fcc9863332c1370920
Peter Donker <peter@bring2mind.net>
2024-05-15 17:15:59 +0200

    Add SPROCs

--- Server/Core/Data/Sprocs.cs
+++ Server/Core/Data/Sprocs.cs
@@ -1,10 +1,40 @@
 using System;
 using System.Collections.Generic;
+using Demo.DnnConnect.Core.Models.Products;
 using DotNetNuke.Data;
 
 namespace Demo.DnnConnect.Core.Data
 {
     public class Sprocs
     {
+        // SELECT
+        //  p.*
+        // FROM dbo.vw_DemoModule_Products p
+        // WHERE p.PortalId=@PortalId
+        // ;  
+        public static IEnumerable<Product> GetProductsByPortal(int portalId)
+        {
+            using (var context = DataContext.Instance())
+            {
+                return context.ExecuteQuery<Product>(System.Data.CommandType.StoredProcedure,
+                    "DemoModule_GetProductsByPortal",
+                    portalId);
+            }
+        }
+
+        // UPDATE dbo.DemoModule_Products
+        // SET ExpiryDate=@NewExpiryDate
+        // WHERE ProductId=@ProductId
+        // ;  
+        public static void SetProductExpiryDate(int productId, DateTime newExpiryDate)
+        {
+            using (var context = DataContext.Instance())
+            {
+                context.Execute(System.Data.CommandType.StoredProcedure,
+                    "DemoModule_SetProductExpiryDate",
+                    productId, newExpiryDate);
+            }
+        }
+
     }
 }
