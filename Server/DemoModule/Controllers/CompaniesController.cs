64f0161797d0efb054779841bb3caac8fca89a18
Peter Donker <peter@bring2mind.net>
2024-05-15 16:52:40 +0200

    Add a bit of MVC frontend to navigate from company to products

---
+++ Server/DemoModule/Controllers/CompaniesController.cs
@@ -0,0 +1,15 @@
+﻿using Demo.DnnConnect.Core.Repositories;
+using Demo.DnnConnect.DemoModule.Common;
+using System.Web.Mvc;
+
+namespace Demo.DnnConnect.DemoModule.Controllers
+{
+  public class CompaniesController : DemoModuleMvcController
+  {
+    [HttpGet]
+    public ActionResult Index(int companyId)
+    {
+      return View(CompanyRepository.Instance.GetCompany(PortalSettings.PortalId, companyId));
+    }
+  }
+}
